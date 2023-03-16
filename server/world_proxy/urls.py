088436c4cfb2bdc741755feae044a62ebb0c9e82
aldotele <aldo.telese@hotmail.it>
2023-03-16 16:23:20 +0100

    adds all countries api

--- server/world_proxy/urls.py
+++ server/world_proxy/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from world_proxy.views import welcome
+from world_proxy.views import welcome, get_all_countries
 
 urlpatterns = [
     path('', welcome, name='welcome'),
+    path('countries/all', get_all_countries, name='get_all_countries')
 ]
--- server/world_proxy/views.py
+++ server/world_proxy/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import requests
+from django.http import HttpResponse
 
 def welcome(request):
     return render(request, 'welcome.html')
+
+def get_all_countries(request):
+    response = requests.get("https://restcountries.com/v3.1/all")
+    return HttpResponse(response, content_type='application/json')
