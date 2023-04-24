795db1707d2888e3a2c63a31e3f829e2c12dc902
aldotele <aldo.telese@hotmail.it>
2023-04-24 14:05:04 +0200

    (poc) adds vercel allowed origin

--- server/world_server/settings.py
+++ server/world_server/settings.py
@@ -62,7 +62,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
-    "http://127.0.0.1:4200"
+    "http://127.0.0.1:4200",
+    "https://globon.vercel.app",
+    "http://globon.vercel.app"
 ]
 
 ROOT_URLCONF = 'world_server.urls'
