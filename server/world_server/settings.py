33231c9f724d5c5f02554387e5087860763ec123
aldotele <aldo.telese@hotmail.it>
2023-04-25 10:56:09 +0200

    adds localhost in allowed hosts

--- server/world_server/settings.py
+++ server/world_server/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["worldproxy.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "worldproxy.onrender.com"]
 
 # Application definition
 
