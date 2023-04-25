1b3e0f92a43f764d4f4cacbe6a8c546b9df9459a
aldotele <aldo.telese@hotmail.it>
2023-04-25 11:36:57 +0200

    adds open api parameters in GET countries

--- server/country/views.py
+++ server/country/views.py
@@ -4,12 +4,15 @@ from rest_framework.views import APIView
 
 from .persistence import get_countries, get_country_by_code
 from .serializers import CountrySerializer
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 
 
 class CountryListView(APIView):
 
-    @extend_schema(responses=CountrySerializer)
+    @extend_schema(responses=CountrySerializer,
+                   parameters=[OpenApiParameter(name="minPopulation", type=int),
+                               OpenApiParameter(name="maxPopulation", type=int),
+                               OpenApiParameter(name="incomeLevel", type=str, enum=["HIC", "UMC", "LMC", "LIC"])])
     def get(self, request):
         filters = self.request.query_params
         queryset = get_countries()
