bb91b66e234eda87296f1f20388057895ffb59e0
aldotele <aldo.telese@hotmail.it>
2023-05-07 17:59:50 +0200

    adds alphabetical sorting to language list

--- server/country/persistence.py
+++ server/country/persistence.py
@@ -14,6 +14,7 @@ def get_all_languages():
     sub_lists = list(Country.objects.values_list('languages', flat=True).distinct())
     list_with_duplicates = itertools.chain.from_iterable(sub_lists)
     unique_ist = list(dict.fromkeys(list_with_duplicates))
+    unique_ist.sort()
     return unique_ist
 
 
