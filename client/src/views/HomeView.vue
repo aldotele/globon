32195604821dd8c51aa5b7e144fada1ddf071b8c
aldotele <aldo.telese@hotmail.it>
2023-07-01 13:17:40 +0200

    adds home view

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,51 @@
 <script setup>
+
+let isReady = false;
+
+// contacting the server
+const launch = () => {
+  fetch("http://localhost:8000/ready/", { method: 'GET', redirect: 'follow'})
+    .then(response => {
+        response.json().then(json => {
+            if (json['ready']) {
+              isReady = true;
+              console.log('ready')
+            }
+        })
+    })
+    .catch(error => {
+      console.log(error);
+        // handle the error
+    });
+}
+
 </script>
 
 <template>
   <main>
+      <h1>The World at your fingertips</h1>
+      <button class="start-btn" @click="launch()">Start Discovering</button>
   </main>
 </template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  .start-btn {
+    height: 45px;
+    width: 250px;
+    margin: 0 auto;
+  }
+}
+</style>
