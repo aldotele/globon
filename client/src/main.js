ed9a842deead2e0e67ea10a7a9e23b2f1f0aef3b
aldotele <aldo.telese@hotmail.it>
2023-08-17 18:59:29 +0200

    adds apollo dependency

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,30 @@
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+// apollo config
+const httpLink = createHttpLink({
+    //uri: 'https://spacex-production.up.railway.app/',
+    uri: 'http://localhost:8000/graphql',
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+})
+
+const app = createApp({
+    setup() {
+        provide(DefaultApolloClient, apolloClient)
+    },
+
+    render: () => h(App),
+})
 
 app.use(router)
 
