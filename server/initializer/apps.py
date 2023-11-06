168958e77518a5a19e4dcc030a9418afea187458
aldotele <aldo.telese@hotmail.it>
2023-11-06 15:07:05 +0100

    activates initialization after migrations

--- server/initializer/apps.py
+++ server/initializer/apps.py
@@ -1,21 +1,26 @@
+import asyncio
+import logging
+
 from django.apps import AppConfig
+
+from initializer.initializer import load_countries
 
 
 class InitializerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'initializer'
 
-    # def ready(self):
-    #     if os.environ.get('RUN_MAIN'):
-    #         logging.info("ONE TIME EXECUTION: populating db ...")
-    #         # import model
-    #         from country.models import Country
-    #
-    #         # check if countries are already on db or not
-    #         if not Country.objects.all():
-    #             asyncio.run(load_countries())
-    #
-    #         return True
-    #     else:
-    #         pass
+    def ready(self):
+        #if os.environ.get('RUN_MAIN'):
+            logging.info("ONE TIME EXECUTION: populating db ...")
+            # import model
+            from country.models import Country
+
+            # check if countries are already on db or not
+            if not Country.objects.all():
+                asyncio.run(load_countries())
+
+            return True
+        #else:
+            pass
 
