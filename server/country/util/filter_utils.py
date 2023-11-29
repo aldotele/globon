0cfeb2dc655947bf88440f42b959e40548f1b3b7
aldotele <aldo.telese@hotmail.it>
2023-11-29 08:46:46 +0100

    simplifies nested field condition on filter

--- server/country/util/filter_utils.py
+++ server/country/util/filter_utils.py
@@ -1,3 +1,3 @@
 def extract_field_value(text: str, delimiter: str) -> tuple:
     text_split = text.split(delimiter)
-    return text_split[0].strip(), text_split[1].strip()
+    return text_split[0].strip().replace(".", "__"), text_split[1].strip()
