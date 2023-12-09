d3f642524c4a5c8740a9109a0ddfe75e323c5ace
aldotele <aldo.telese@hotmail.it>
2023-12-09 16:17:48 +0100

    adds documentation for graphql UI

--- server/city/schema.py
+++ server/city/schema.py
@@ -11,10 +11,20 @@ class CityType(DjangoObjectType):
     class Meta:
         model = City
         fields = "__all__"
+        description = "Note that fields are in camelCase, " \
+                      "but if you wish to use them inside the optional <search> parameter " \
+                      "you must specify them in kebab_case: adminName --> admin_name"
 
 
 class Query(graphene.ObjectType):
-    cities = graphene.List(CityType, search=graphene.String())
+    cities = graphene.List(CityType,
+                           search=graphene.String(description='the search filter must respect the following format: '
+                                                              '"<field> <operator> <value>" '
+                                                              'where <operator> can be one of [=, <, >, contains]. '
+                                                              'For example one valid filter is: '
+                                                              '"iso3 = ITA & population > 1000000". Note that you '
+                                                              'can use multiple filters separated by &.'),
+                           description= "query to retrieve cities, with an optional search parameter acting as a filter")
 
     def resolve_cities(self, info, search=""):
         if search:
