421f73b2f338da12c0ca6fa925238eb303f5d1b5
aldotele <aldo.telese@hotmail.it>
2024-01-27 11:32:54 +0100

    run initializater

--- server/initializer/apps.py
+++ server/initializer/apps.py
@@ -11,16 +11,23 @@ class InitializerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'initializer'
 
-    def ready(self):
-        if os.environ.get('RUN_MAIN'):
-            logging.info("ONE TIME EXECUTION: populating db ...")
-            # import model
-            from country.models import Country
-            # check if countries are already on db or not
-            if not Country.objects.all():
-                asyncio.run(load_countries())
+    def ready(self):        
+        # import model
+        from country.models import Country
+        # check if countries are already on db or not
+        if not Country.objects.all():
+            asyncio.run(load_countries())
+        return True
+        
+        # if os.environ.get('RUN_MAIN'):
+        #     logging.info("ONE TIME EXECUTION: populating db ...")
+        #     # import model
+        #     from country.models import Country
+        #     # check if countries are already on db or not
+        #     if not Country.objects.all():
+        #         asyncio.run(load_countries())
 
-            return True
-        else:
-            pass
+        #     return True
+        # else:
+        #     pass
 
